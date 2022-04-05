1c733ee65e0910b5cb106aafbb8d6309c6b9f34d
Antonio Paulino <tonnyarauujo@gmail.com>
2022-04-05 14:14:36 -0300

    add error handler to service

--- app/services/share_price_service.rb
+++ app/services/share_price_service.rb
@@ -1,13 +1,18 @@
 class SharePriceService
   FLOAT_TO_INT_CONVERSION = 100
-
+  BASE_URL = "https://statusinvest.com.br/acoes/"
+  
   def initialize(asset)
     @asset = asset
   end
   
   def get_share_price
-    uri = URI.parse("https://statusinvest.com.br/acoes/#{@asset.downcase}")
-    html = Nokogiri::HTML(Net::HTTP.get_response(uri).body)
-    price = html.css("strong").first.text.gsub(",", ".").to_f*FLOAT_TO_INT_CONVERSION
+    begin
+      uri = URI.parse("#{BASE_URL}#{@asset.downcase}")
+      html = Nokogiri::HTML(Net::HTTP.get_response(uri).body)
+      price = html.css("strong").first.text.gsub(",", ".").to_f*FLOAT_TO_INT_CONVERSION
+    rescue StandardError
+      "URL can't be found"
+    end
   end
 end
