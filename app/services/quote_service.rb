ce91e5fc16d885e27a2c98ae2b2feadb05836bfc
Antonio Paulino <tonnyarauujo@gmail.com>
2022-04-07 10:09:49 -0300

    add has_many association between models

--- app/services/quote_service.rb
+++ app/services/quote_service.rb
@@ -6,7 +6,7 @@ class QuoteService
   def quote_update
     price = SharePriceService.new(@asset_symbol).get_share_price
     asset = Asset.find_by!(symbol: @asset_symbol)
-    Quote.where(asset_id: asset.id).update_all(current: false)
-    Quote.create!(price: price, current: true, asset_id: asset.id)
+    asset.quotes.update_all(current: false)
+    asset.quotes.create!(price: price, current: true, asset_id: asset.id)
   end
 end
