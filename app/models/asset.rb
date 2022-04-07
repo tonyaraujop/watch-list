aabe11153eeb246d8d9500aced4f33ebd34b5dd0
Antonio Paulino <tonnyarauujo@gmail.com>
2022-04-07 16:50:46 -0300

    Add sidekiq queue priority

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,5 +2,6 @@ class Asset < ApplicationRecord
   validates :symbol, presence: true, uniqueness: true
 
   has_many :quotes
+  has_many :wallet_items
   has_one :last_quote, -> { where(quotes: { current: true }) }, class_name: 'Quote'
 end
--- app/services/assets_quotes_service.rb
+++ app/services/assets_quotes_service.rb
@@ -1,7 +1,8 @@
 class AssetsQuotesService
   def update_assets_quotes
     Asset.all.each do |asset|
-      SharePriceJob.perform_async(asset.symbol)
+      queue_type = asset.wallet_items.present? ? :critical : :default
+      SharePriceJob.set(queue: queue_type).perform_async(asset.symbol)
     end
   end
 end
